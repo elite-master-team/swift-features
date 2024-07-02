b5e233753c0d84a7d54132ede0f470717d5912ad
Willians Martins <professorwillians@gmail.com>
2024-07-01 20:22:20 -0700

    Initial Commit

---
+++ Shared/projeto_via_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  projeto_via_xcodeApp.swift
+//  Shared
+//
+//  Created by User on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct projeto_via_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
